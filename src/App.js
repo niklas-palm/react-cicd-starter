78f92f2c2597af86586559ecbd27954ff81d6bba
robot <robot@example.com>
2020-12-18 18:18:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styling/app.scss";
+
+function App() {
+  return (
+    <div className="AppContainer">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
